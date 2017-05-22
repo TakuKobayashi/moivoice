3ffcbd115c1794a64bc23b677bfbb7f6a0080a8d
TakuKobayshi <takukobayshi@TakuKobayshis-iMac.local>
2017-05-22 14:27:05 +0900

    Revert "一旦activerecordがなくても動くようにしておく"
    This reverts commit 541364c56afd7a5425f51e89fc72779b72f50f87.

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,6 @@
 require_relative 'boot'
 
-# DB を使わないので一旦コメントアウト
-#require 'rails/all'
-
-require "rails"      
-require "active_job/railtie"        
-require "action_controller/railtie"     
-require "action_mailer/railtie"     
-require "action_view/railtie"       
-require "action_cable/engine"       
-require "sprockets/railtie"       
-require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -22,7 +12,7 @@ module Moivoice
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = "Tokyo"
-    #config.active_record.default_timezone = :local
+    config.active_record.default_timezone = :local
 
     config.autoload_paths << Rails.root.join('lib')
     config.encoding = "utf-8"
